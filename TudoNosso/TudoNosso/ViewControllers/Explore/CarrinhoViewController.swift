1bcad2b378f43c194f5c8134ef4af32215ec451b
Joao Victor Flores <j199818@dac.unicamp.br>
2020-07-28 06:34:15 -0300

    cellProductsBuy

--- TudoNosso/TudoNosso/ViewControllers/Explore/CarrinhoViewController.swift
+++ TudoNosso/TudoNosso/ViewControllers/Explore/CarrinhoViewController.swift
@@ -8,29 +8,13 @@
 
 import UIKit
 
-class CarrinhoViewController: UIViewController {
+class CarrinhoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    //  MARK: - IBAction
     @IBOutlet weak var buttonSend: UIButton!
+    @IBOutlet weak var tableItens: UITableView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        buttonSend.layer.cornerRadius = 10
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-
-        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-        //tap.cancelsTouchesInView = false
-
-        view.addGestureRecognizer(tap)
-        // Do any additional setup after loading the view.
-    }
-    
-    @objc func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
-    }
-    
+    //  MARK: - IBAction
     @IBAction func sendCarrinho(_ sender: Any) {
       
         var str =
@@ -79,4 +63,71 @@ class CarrinhoViewController: UIViewController {
         print("limpando carrinho")
     }
     
+    //  MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        buttonSend.layer.cornerRadius = 10
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+
+        tableItens.delegate = self
+        tableItens.dataSource = self
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+        view.addGestureRecognizer(tap)
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    //  MARK: - TableView
+    let cellIdentifier = "CellIdentifier"
+    
+    //tags
+    let titleTag = 1000
+    let addsTag = 1001
+    let obsTag = 1002
+    let priceTag = 1003
+    let unitsTag = 1004
+    let viewTag = 100
+    let imageTag = 10
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
+         
+        // Fetch Fruit
+        let fruit = fruits[indexPath.row]
+
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = fruit
+        
+        
+        
+        return cell
+    }
+    
+    let fruits = ["Apple", "Pineapple", "Orange", "Blackberry", "Banana", "Pear", "Kiwi", "Strawberry", "Mango", "Walnut", "Apricot", "Tomato", "Almond", "Date", "Melon"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numberOfRows = fruits.count
+        return numberOfRows
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
 }
