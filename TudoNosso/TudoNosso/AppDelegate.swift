b8df4d12a8c3c102957a806c6ea3a48efdb8f1d2
Joao Victor Flores <j199818@dac.unicamp.br>
2020-07-30 11:41:02 -0300

    Delete

--- TudoNosso/TudoNosso/AppDelegate.swift
+++ TudoNosso/TudoNosso/AppDelegate.swift
@@ -19,14 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        
-        if Local.userMail == nil {  // no logged account
+//        window = UIWindow(frame: UIScreen.main.bounds)
 
-            ViewUtilities.navigateToStoryBoard(storyboardName: "Explore", storyboardID: "Explore", window: window)
-        } else {
-            ViewUtilities.navigateToStoryBoard(storyboardName: "Main", storyboardID: "Tab", window: window)
-        }
+		//ViewUtilities.navigateToStoryBoard(storyboardName: "Explore", storyboardID: "Explore", window: window)
         
         return true
     }
--- TudoNosso/TudoNosso/Extensions/UIColor+Extension.swift
+++ TudoNosso/TudoNosso/Extensions/UIColor+Extension.swift
@@ -9,21 +9,7 @@
 import UIKit
 
 extension UIColor {
-   
-    class var bg: UIColor { return UIColor(rgb: 0xF9F9F9)}
-    class var thinGray: UIColor { return UIColor(rgb: 0xD4D4D4)}
-    class var lightGray: UIColor { return UIColor(rgb: 0xA9A9A9)}
-    class var text: UIColor { return UIColor(rgb: 0x515151)}
-    class var appBlack: UIColor { return UIColor(rgb: 0x212121)}
-    class var darkOrange: UIColor { return UIColor(rgb: 0xE45102)}
-    class var orange: UIColor { return UIColor(rgb: 0xFF5900)}
-    static var primary: UIColor {
-      return UIColor(red: 216 / 255, green: 242 / 255, blue: 173 / 255, alpha: 1)
-    }
-    
-    static var incomingMessage: UIColor {
-      return UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1)
-    }
+
     convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
         self.init(
             red: CGFloat(red) / 255.0,
