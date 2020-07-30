9d1a0067b7eec4f451e3a59e5de4ff89d67fc444
Joao Victor Flores <j199818@dac.unicamp.br>
2020-07-30 10:51:26 -0300

    Delete

--- TudoNosso/TudoNosso/Extensions/SenderType+extensions.swift
+++ TudoNosso/TudoNosso/Extensions/SenderType+extensions.swift
@@ -1,24 +1,24 @@
+////
+////  SenderType+extensions.swift
+////  TudoNosso
+////
+////  Created by Bruno Cardoso Ambrosio on 22/11/19.
+////  Copyright © 2019 Joao Flores. All rights reserved.
+////
 //
-//  SenderType+extensions.swift
-//  TudoNosso
+//import Foundation
+//import MessageKit
 //
-//  Created by Bruno Cardoso Ambrosio on 22/11/19.
-//  Copyright © 2019 Joao Flores. All rights reserved.
-//
-
-import Foundation
-import MessageKit
-
-extension SenderType {
-    var initials:String{
-        let names = displayName.split(separator: " ")
-        var initials = ""
-        names.forEach { (name) in
-            let index = name.index(name.startIndex, offsetBy: 1)
-            let mySubstring = name[..<index] // Hello
-            initials += String(mySubstring.uppercased())
-        }
-        
-        return initials
-    }
-}
+//extension SenderType {
+//    var initials:String{
+//        let names = displayName.split(separator: " ")
+//        var initials = ""
+//        names.forEach { (name) in
+//            let index = name.index(name.startIndex, offsetBy: 1)
+//            let mySubstring = name[..<index] // Hello
+//            initials += String(mySubstring.uppercased())
+//        }
+//        
+//        return initials
+//    }
+//}
