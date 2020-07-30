4eec5966957a32eb2fafcdc427a70435431859f9
Joao Victor Flores <j199818@dac.unicamp.br>
2020-07-30 11:22:24 -0300

    Delete

--- TudoNosso/TudoNosso/Models/DatabaseRepresentation.swift
+++ TudoNosso/TudoNosso/Models/DatabaseRepresentation.swift
@@ -1,20 +1,20 @@
+////
+////  DatabaseRepresentation.swift
+////  TudoNosso
+////
+////  Created by Bruno Cardoso Ambrosio on 06/11/19.
+////  Copyright © 2019 Joao Flores. All rights reserved.
+////
 //
-//  DatabaseRepresentation.swift
-//  TudoNosso
+//import Foundation
 //
-//  Created by Bruno Cardoso Ambrosio on 06/11/19.
-//  Copyright © 2019 Joao Flores. All rights reserved.
+///// The class that use this protocol will need to inform a how it is described in the database 
+//protocol DatabaseRepresentation {
+//    
+//    
+//    var representation: [String: Any] { get }
 //
-
-import Foundation
-
-/// The class that use this protocol will need to inform a how it is described in the database 
-protocol DatabaseRepresentation {
-    
-    
-    var representation: [String: Any] { get }
-
-    
-}
-
-
+//    
+//}
+//
+//
